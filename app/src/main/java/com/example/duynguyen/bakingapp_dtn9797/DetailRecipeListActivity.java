28857e6513f205fc6c2b5f1b65921b49237b849e
Duy Nguyen <dtn9797@gmail.com>
2018-06-29 23:37:29 -0500

    Added DetailRecipeListActivity

---
+++ app/src/main/java/com/example/duynguyen/bakingapp_dtn9797/DetailRecipeListActivity.java
@@ -0,0 +1,18 @@
+package com.example.duynguyen.bakingapp_dtn9797;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by duynguyen on 6/29/18.
+ */
+
+public class DetailRecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_recipe_list);
+    }
+}
