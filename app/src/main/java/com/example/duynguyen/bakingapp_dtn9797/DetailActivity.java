7ba379d595430e92804691c91f6e660f1d96e86e
Duy Nguyen <dtn9797@gmail.com>
2018-07-06 19:31:36 -0500

    Show UI in 2 pane & fix bug about displaying black screen

--- app/src/main/java/com/example/duynguyen/bakingapp_dtn9797/DetailActivity.java
+++ app/src/main/java/com/example/duynguyen/bakingapp_dtn9797/DetailActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.example.duynguyen.bakingapp_dtn9797.fragments.IngredientsFragment;
 import com.example.duynguyen.bakingapp_dtn9797.fragments.ListFragment;
+import com.example.duynguyen.bakingapp_dtn9797.fragments.PlayerFragment;
 import com.example.duynguyen.bakingapp_dtn9797.model.Recipe;
+import com.example.duynguyen.bakingapp_dtn9797.model.Step;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by duynguyen on 6/29/18.
@@ -20,14 +25,18 @@ public class DetailActivity extends AppCompatActivity implements ListFragment.On
 
     public static String RECIPE_EXTRA = "recipe_extra";
 
+    private boolean mTwoPane;
+    android.support.v4.app.FragmentManager mFragmentManager;
+
     Recipe recipe;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        mFragmentManager = getSupportFragmentManager();
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             Intent intent = getIntent();
             if (intent == null) {
                 closeOnError();
@@ -35,13 +44,13 @@ public class DetailActivity extends AppCompatActivity implements ListFragment.On
 
             recipe = intent.getParcelableExtra(RECIPE_EXTRA);
 
-            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+
             Bundle bundle = new Bundle();
             bundle.putStringArrayList(ListFragment.LIST_NAMES_EXTRA, recipe.getShortDescriptionsFromSteps());
             ListFragment listFragment = new ListFragment();
             listFragment.setArguments(bundle);
 
-            fragmentManager.beginTransaction()
+            mFragmentManager.beginTransaction()
                     .add(R.id.detail_recipe_list_fragment, listFragment)
                     .commit();
         } else {
@@ -49,7 +58,8 @@ public class DetailActivity extends AppCompatActivity implements ListFragment.On
         }
 
         setTitle(recipe.getName());
-
+        //Check for tablet layout
+        mTwoPane = (findViewById(R.id.step_player_fragment) != null) ? true : false;
     }
 
     private void closeOnError() {
@@ -60,17 +70,47 @@ public class DetailActivity extends AppCompatActivity implements ListFragment.On
 
     @Override
     public void onItemSelected(int position) {
-        if (position == 0){
-            Intent ingredientsIntent = new Intent(DetailActivity.this,IngredientsActivity.class);
-            ingredientsIntent.putParcelableArrayListExtra(IngredientsActivity.INGREDIENTS_EXTRA, (ArrayList<? extends Parcelable>) recipe.getIngredients());
-            startActivity(ingredientsIntent);
+        if (!mTwoPane) {
+            if (position == 0) {
+                Intent ingredientsIntent = new Intent(DetailActivity.this, IngredientsActivity.class);
+                ingredientsIntent.putParcelableArrayListExtra(IngredientsActivity.INGREDIENTS_EXTRA, (ArrayList<? extends Parcelable>) recipe.getIngredients());
+                startActivity(ingredientsIntent);
+            } else {
+                Intent stepIntent = new Intent(DetailActivity.this, StepActivity.class);
+                stepIntent.putParcelableArrayListExtra(StepActivity.STEPS_EXTRA, (ArrayList<? extends Parcelable>) recipe.getSteps());
+                stepIntent.putExtra(StepActivity.POS_EXTRA, position - 1);
+                stepIntent.putExtra(StepActivity.RECIPE_NAME_EXTRA, recipe.getName());
+                startActivity(stepIntent);
+            }
         }
         else {
-            Intent stepIntent = new Intent(DetailActivity.this,StepActivity.class);
-            stepIntent.putParcelableArrayListExtra(StepActivity.STEPS_EXTRA, (ArrayList<? extends Parcelable>) recipe.getSteps());
-            stepIntent.putExtra(StepActivity.POS_EXTRA,position-1);
-            stepIntent.putExtra(StepActivity.RECIPE_NAME_EXTRA,recipe.getName());
-            startActivity(stepIntent);
+            if (position == 0) {
+                IngredientsFragment ingredientsFragment = new IngredientsFragment();
+                Bundle bundle = new Bundle();
+                bundle.putParcelableArrayList(IngredientsFragment.INGREDIENTS_EXTRA, (ArrayList<? extends Parcelable>) recipe.getIngredients());
+                ingredientsFragment.setArguments(bundle);
+
+                mFragmentManager.beginTransaction()
+                        .replace(R.id.step_player_fragment,ingredientsFragment)
+                        .commit();
+            }else{
+                List<Step> steps = recipe.getSteps();
+                int stepPos = position - 1;
+                String url0 = steps.get(stepPos).getVideoURL();
+                String url1 = steps.get(stepPos).getThumbnailURL();
+                String videoUrl = (url1.equals(""))?url0:url1;
+                String stepDescription = steps.get(stepPos).getDescription();
+
+                Bundle bundle = new Bundle();
+                bundle.putString(PlayerFragment.DESCRIPTION_EXTRA,stepDescription);
+                bundle.putString(PlayerFragment.VIDEO_URL_EXTRA,videoUrl);
+                PlayerFragment playerFragment = new PlayerFragment();
+                playerFragment.setArguments(bundle);
+
+                mFragmentManager.beginTransaction()
+                        .replace(R.id.step_player_fragment, playerFragment)
+                        .commit();
+            }
         }
     }
 
